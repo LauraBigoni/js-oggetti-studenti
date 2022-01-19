78b507fcf313ad65334144f754927e126459c381
Laura Bigoni <laurabigoni.92@gmail.com>
2022-01-19 15:38:59 +0100

    added initial files

---
+++ js/script.js
@@ -0,0 +1,16 @@
+Descrizione:
+/*
+TRACCIA:
+1. Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+2. Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+3. Creare un array di oggetti di studenti (ossìa oggetti con le proprietà sopra descritte).
+4. Ciclare su tutti gli studenti e stampare per ognuno di essi: nome e cognome.
+5. Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere all’array creato in precedenza, un nuovo oggetto studente inserendo nell’ ordine: nome, cognome e età.
+
+BONUS:
+Usiamo il DOM per stampare e chiedere le informazioni all'utente!
+Consigli del giorno
+- Scriviamo sempre prima in italiano i passaggi che vogliamo fare
+- Scriviamo sempre solo un pezzetto di codice alla volta e verifichiamo (console.log), se funziona allora andiamo avanti.
+*/
+
